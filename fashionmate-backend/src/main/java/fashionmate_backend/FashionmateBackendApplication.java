c150c40e24901a7d83012750b9229232500be7eb
swarnaburra <swarnaburra2025@gmail.com>
2026-02-24 23:16:12 -0600

    PROJECT SETUP & DATABASE CONFIGURATION

---
+++ fashionmate-backend/src/main/java/fashionmate_backend/FashionmateBackendApplication.java
@@ -0,0 +1,13 @@
+package fashionmate_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionmateBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(fashionmate_backend.FashionmateBackendApplication.class, args);
+	}
+
+}
---
+++ fashionmate-backend/src/main/java/fashionmate_backend/models/Review.java
@@ -0,0 +1,4 @@
+package fashionmate_backend.models;
+
+public class Review {
+}
