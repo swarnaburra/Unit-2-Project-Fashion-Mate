a3a370e00aef2de090ac09f97ed370fe3178c62e
swarnaburra <swarnaburra2025@gmail.com>
2026-03-13 23:10:27 -0500

    Updated the background image

--- fashionmate-frontend/fashion-app/src/components/Footer.jsx
+++ fashionmate-frontend/fashion-app/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
     return (
         <footer>
-            <p> &copy; 2025 FashionMate. All Rights Reserved.</p>
+            <p> &copy; 2026 FashionMate. All Rights Reserved.</p>
         </footer>
 
     );
